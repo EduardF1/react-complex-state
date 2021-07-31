ba71d7f97695c87b0168a7f7569aeed4a0adb310
EduardF1 <fischer_eduard@yahoo.com>
2021-07-31 14:11:29 +0200

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,51 @@
+import React, {useState} from "react";
+
+function App() {
+
+    const [fullName, setFullName] = useState({
+        fName: '',
+        lName: ''
+    });
+
+    function handleChange(event: any) {
+        const {value, name} = event.target;
+
+        setFullName((previousValue => {
+            if (name === 'fName') {
+                return ({
+                    fName: value,
+                    lName: previousValue.lName
+                });
+            } else if (name === 'lName') {
+                return ({
+                    fName: previousValue.fName,
+                    lName: value
+                });
+            } else {
+                return ({
+                    fName: '',
+                    lName: ''
+                });
+            }
+        }))
+    }
+
+    return (
+        <div className="container">
+            <h1>Hello {fullName.fName} {fullName.lName}</h1>
+            <form>
+                <input name="fName"
+                       onChange={handleChange}
+                       placeholder="First Name"
+                       value={fullName.fName}/>
+                <input name="lName"
+                       onChange={handleChange}
+                       placeholder="Last Name"
+                       value={fullName.lName}/>
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default App;
